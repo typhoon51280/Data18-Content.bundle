f655920ccebeee86f026b96544eb1080752bb29d
Freddy Knuth <frankiedreamsof@gmail.com>
2015-07-03 20:18:21 -0500

    Fix xPath given new Data18 update

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -37,7 +37,7 @@ def search_na(results, media_title, year, lang):
     searchURL = EXC_BASEURL + 'dev/' + query_actor
     Log('Search URL: ' + searchURL)
     search_results = HTML.ElementFromURL(searchURL)
-  
+
   xp = '''//select//Option[text()[contains(translate(., "%s", "%s"), "%s")]]//@value''' % (na_url_site.upper(), na_url_site.lower(), na_url_site.lower())
   Log('xPath: ' + xp)
   try:
@@ -52,9 +52,9 @@ def search_na(results, media_title, year, lang):
     xp = '''//a[text()[contains(translate(., "%s", "%s"), "%s")]]//@href''' % (na_url_site.upper(), na_url_site.lower(), na_url_site.lower())
     Log('xPath: ' + xp)
     searchURL = search_results.xpath(xp)[0]
-    
+
   #Log('Search URL: ' + searchURL)
-  search_results = HTML.ElementFromURL(searchURL)    	
+  search_results = HTML.ElementFromURL(searchURL)
 
 
   #searchURL = EXC_BASEURL + query_actor + '/sites/' + na_url_part + '.html'
@@ -63,7 +63,7 @@ def search_na(results, media_title, year, lang):
     #search_results = HTML.ElementFromURL(searchURL)
   #except:
     #searchURL = EXC_BASEURL +'dev/' + query_actor + '/sites/' + na_url_part + '.html'
-    #search_results = HTML.ElementFromURL(searchURL)    	
+    #search_results = HTML.ElementFromURL(searchURL)
   count = 0
   for movie in search_results.xpath('//div[@class="bscene2 genmed"]//p[@class="line1"]//a[@class="gen11 bold"]'):
     movie_HREF = movie.get("href").strip()
@@ -72,7 +72,7 @@ def search_na(results, media_title, year, lang):
     Log('New title: ' + current_name)
     current_ID = movie.get('href').split('/',4)[4]
     Log('New ID: ' + current_ID)
-    
+
     try:
       movieResults = HTML.ElementFromURL(movie_HREF)
       curyear = movieResults.xpath('//p[contains(text(),"Date")]//a')[0].get('href')
@@ -111,7 +111,7 @@ def search_na(results, media_title, year, lang):
       results.Append(MetadataSearchResult(id = current_ID, name = current_name, score = score, lang = lang))
     count += 1
   results.Sort('score', descending=True)
-  
+
 
 class EXCAgent(Agent.Movies):
   name = 'Data18-Content'
@@ -143,20 +143,20 @@ class EXCAgent(Agent.Movies):
 
     if " in " in title.lower():
       search_na(results, title, year, lang)
-      
-        
+
+
     if len(results) == 0:
       #query = String.URLEncode(String.StripDiacritics(title.replace('-','')))
       query = String.URLEncode(String.StripDiacritics(title))
-      
+
       searchUrl = EXC_SEARCH_MOVIES % query
       Log('search url: ' + searchUrl)
       searchResults = HTML.ElementFromURL(searchUrl)
       searchTitle = searchResults.xpath('//title')[0].text_content()
       count = 0
-      for movie in searchResults.xpath('//div[@class="gen"]//p[@class="gen12"]//a[contains(@href,"content")]'):
+      for movie in searchResults.xpath('//div[@class="gen"]//span//a[contains(@href,"content")]'):
         movieHREF = movie.get("href").strip()
-        Log('MovieHREF: ' + movieHREF)     
+        Log('MovieHREF: ' + movieHREF)
         curName = movie.text_content().strip()
         Log('newTitle: ' + curName)
         curID = movie.get('href').split('/',4)[4]
@@ -200,9 +200,9 @@ class EXCAgent(Agent.Movies):
         count += 1
       results.Sort('score', descending=True)
 
-    
-    
-    
+
+
+
 
   def update(self, metadata, media, lang):
     Log('Data18 Version : ' + VERSION_NO)
@@ -321,7 +321,7 @@ class EXCAgent(Agent.Movies):
       Log('Art -Embedded Video- Sequence Updated')
     except: pass
 
- # Get First Photo Set Pic if available
+    # Get First Photo Set Pic if available
     try:
       photoSetIndex = 0
       imageURL =  html.xpath('//img[contains(@alt,"image")]/..')[photoSetIndex].get('href')
